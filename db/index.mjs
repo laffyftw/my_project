5629fa1d7d33ac072f83484a30bca3db79f2b442
laffyftw <laspestrand@gmail.com>
2023-03-27 07:01:57 +0100

    changing process.env

--- db/index.mjs
+++ db/index.mjs
@@ -3,11 +3,10 @@ import pg from 'pg';
 const { Pool } = pg;
 
 const pool = new Pool({
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-  });
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false,
+    },
+});
 
 export default pool;
